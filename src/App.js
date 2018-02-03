f04962eff938a565d4a4e4a696b2bf6d04708bb7
C Mccann <mccannsean421@gmail.com>
2018-02-03 13:57:30 +0000

    initial

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Movie, { component } from './Movie'
+import logo from './logo.svg';
+import './App.css';
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  },
+  {
+    id: 2,
+    title: 'Spider Man'
+  },
+  {
+    id: 3,
+    title: 'Men In Black'
+  },
+]
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        {movies.map((movie) => (
+            <Movie key={movie.id} movie={movie} />
+          ))}
+      </div>
+    );
+  }
+}
+
+
+export default App;
