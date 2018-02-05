f62fbe2e66d2e4f824803eb08fdeb39a13bd1b21
C Mccann <mccannsean421@gmail.com>
2018-02-05 07:46:31 +0000

    static PropTypes and Default Props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import './App.css';
 const movies = [
   {
     id: 1,
-    title: 'Star Wars'
+    title: 'Star Wars',
+    desc: 'Space movie'
   },
   {
     id: 2,
@@ -27,7 +28,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         {movies.map((movie) => (
-            <Movie key={movie.id} movie={movie} />
+            <Movie key={movie.id} movie={movie} desc={movie.desc} />
           ))}
       </div>
     );
