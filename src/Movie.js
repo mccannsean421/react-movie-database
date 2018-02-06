6f6a6133c209a18b1681f84fcae6220767595129
C Mccann <mccannsean421@gmail.com>
2018-02-06 18:11:53 +0000

    functionalstateless components

--- src/Movie.js
+++ src/Movie.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export default class Movie extends Component {
-    static propTypes = {
-      movie: PropTypes.shape({
-         title: PropTypes.string.isRequired,
-      })
-    }
-  
-    render() {
-      return (
-        <div >
-            <h3>{this.props.movie.title}</h3>
-        </div>
-      );
-    }
-  }
+const Movie = ({ movie }) => (
+  <div >
+    <h3>{movie.title}</h3>
+  </div>
+);
+
+export default Movie;
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }),
+};
